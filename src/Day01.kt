9a0385d25c4d6e3de73c99fd0647b49ec3ce34a8
Tshiamo Bhuda <tshiamobhuda@gmail.com>
2021-12-01 10:25:17 +0200

    Added solution for day 1 part 2

--- src/Day01.kt
+++ src/Day01.kt
@@ -21,15 +21,36 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var index = 0
+        var increases = 0
+        var prevDepthWithWindows = 0
+        while (index < input.size - 2) {
+            val regex = Regex("(\\d+)")
+            val window1 = regex.find(input[index])?.value?.toInt() ?: 0
+            val window2 = regex.find(input[index +1])?.value?.toInt() ?: 0
+            val window3 = regex.find(input[index + 2])?.value?.toInt() ?: 0
+            val sum = window1 + window2 + window3
+
+            if (sum > prevDepthWithWindows && index != 0) {
+                increases++
+            }
+
+            prevDepthWithWindows = sum
+            index++
+        }
+
+        return increases
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
     check(part1(testInput) == 7)
-    println(part1(testInput))
+    println("[TEST]-part1: ${part1(testInput)}")
+    check(part2(testInput) == 5)
+    println("[TEST]-part2: ${part2(testInput)}")
 
     val input = readInput("Day01")
-    println(part1(input))
-//    println(part2(input))
+    println("--- AOC 2021 ---")
+    println("[ANSWER]-part1: ${part1(input)}")
+    println("[ANSWER]-part2: ${part2(input)}")
 }
